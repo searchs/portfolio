cbccb2875dcd2bb3e04c341b01dc6456894e4979
searchs <ola.ajibode@gmail.com>
2023-08-03 17:47:17 +0100

    Job app added. Switch db to pg

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "blog.apps.BlogConfig",
     "jobs.apps.JobsConfig",
     "django.contrib.admin",
     "django.contrib.auth",
@@ -55,7 +56,7 @@ ROOT_URLCONF = "portfolio.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": ["templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -76,8 +77,12 @@ WSGI_APPLICATION = "portfolio.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "portfoliodb",
+        "USER": "screative",
+        "PASSWORD": "password",
+        "HOST": "localhost",
+        "PORT": "5433",
     }
 }
 
---
+++ portfolio/views.py
@@ -0,0 +1,8 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+
+
+@csrf_exempt
+def home(request):
+    return render(request, "home.html")
